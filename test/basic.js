e3f37994052707066946bbe04f0c690df3159ab6
Roman Shtylman <shtylman@gmail.com>
2012-08-13 18:22:45 +0100

    refactor
    - move browser and service into separate files
    - constants file for config
    - fix monitoring online status by having the service emit answers on an
      interval

--- test/basic.js
+++ test/basic.js
@@ -7,15 +7,19 @@ test('updown', function(done) {
     var service = spaceport.service('test', { port: 1234 }).start();
     var browser = spaceport.browser('test').start();
 
+    var ident;
+
     browser.on('up', function(info) {
         assert.equal(info.port, 1234);
         assert.equal(info.host, k_hostname);
 
+        ident = info.ident;
         service.stop();
     });
 
-    browser.on('down', function() {
+    browser.on('down', function(info) {
         browser.stop();
+        assert.equal(ident, info.ident);
         done();
     });
 });
